42c61335868ce55bff49d07f0ea77484276ef181
Vikingo9311 <carlosdarias1993@gmail.com>
2026-04-11 08:00:42 +0200

    cors abierto

--- server.js
+++ server.js
@@ -12,14 +12,8 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(cors());
+app.use(cors({origin: "*"}));
 app.use(express.json());
-app.use(cors({
-  origin: [
-    "http://localhost:5500",
-    "http://127.0.0.1:5500"
-  ]
-}));
 // Datos en memoria
 let reservas = [];
 
